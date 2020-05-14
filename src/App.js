30a5631597b2be457a8dd571ee8bbba237f483f8
emilie <emi.m.humbert@googlemail.com>
2020-05-14 15:18:55 +0100

    css and handle input

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "./components/Search";
 
 function App() {
+  const [state, setState]  = useState({
+    s: "",
+    results: [],
+    selected: {},
+  });
+  const apiurl = "http://www.omdbapi.com/?i=tt3896198&apikey=73f42ff7";
+
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s };
+    });
+
+    console.log(state.s);
+  };
+
   return (
     <div className="App">
       <header>
         <h1>Movie database</h1>
       </header>
+      <main>
+        <Search handleInput={handleInput} />
+      </main>
     </div>
   );
 }
